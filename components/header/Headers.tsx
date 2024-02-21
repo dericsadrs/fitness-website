344f867f7cd102ad8da2b49ba9e8565f50ec86b4
Deric <105538170+dericsadrs@users.noreply.github.com>
2024-02-21 21:14:26 +0800

    Add headers

--- components/header/Headers.tsx
+++ components/header/Headers.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { headerLinksData } from "../../data/data";
-
+import logo from "../../assets/Icon.png";
 import styles from "./header.css";
-
-
 
 const Header = () => {
     const navigate = useNavigate();
@@ -13,7 +11,41 @@ const Header = () => {
 
     const menuHandler = () => {
         setActive(!active);
-    }
+    };
 
-    return 
-}
+    return (
+        <header>
+          <div className={`${styles.header_content} container`}>
+            <div className={styles.logo_holder} onClick={() => navigate("/")}>
+              <div className={styles.image_holder}>
+                <img src={logo} alt="Logo" />
+              </div>
+              Strengthy
+            </div>
+            <ul className={active ? `${styles.activeList}` : ""}>
+              {headerLinksData.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <Link to={link.to}>{link.title}</Link>
+                  </li>
+                );
+              })}
+    
+              <li>
+                <button onClick={() => navigate("/classes")}>Book Class</button>
+              </li>
+            </ul>
+            <div className={styles.menu} onClick={() => menuHandler()}>
+              <i className={`fas ${active ? "fa-times" : "fa-bars"} open-list`}></i>
+            </div>
+            <div
+              className={`${styles.menu_overlay} ${
+                active ? `${styles.active_overlay}` : ""
+              }`}
+              onClick={() => menuHandler()}></div>
+          </div>
+        </header>
+      );
+};
+
+export default Header;
